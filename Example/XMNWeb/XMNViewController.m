288a3c31532471644e43208c328311250e2bb488
ChenMaolei <XMFraker@ChenMaoleideMacBook-Pro.local>
2017-02-17 09:45:47 +0800

    更新测试工程

--- Example/XMNWeb/XMNViewController.m
+++ Example/XMNWeb/XMNViewController.m
@@ -8,7 +8,10 @@
 
 #import "XMNViewController.h"
 
+#import "XMNWebController.h"
+
 @interface XMNViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 
 @end
 
@@ -18,12 +21,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+#ifdef XMNBRIDGE_ENABLED
+    NSLog(@"this framework contains jsbridge");
+#endif
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)openWebAction:(UIButton *)sender {
+    
+    XMNWebController *webC = [[XMNWebController alloc] initWithURL:[NSURL URLWithString:self.urlTextField.text]];
+    
+    if (webC) {
+        [self.navigationController pushViewController:webC animated:YES];
+    }
+}
+- (IBAction)openLocalFile:(UIButton *)sender {
+    
+    
+    XMNWebController *webC = [[XMNWebController alloc] initWithURL:[[NSBundle mainBundle] URLForResource:@"test.html" withExtension:nil]];
+    
+    if (webC) {
+        [self.navigationController pushViewController:webC animated:YES];
+    }
 }
 
 @end
